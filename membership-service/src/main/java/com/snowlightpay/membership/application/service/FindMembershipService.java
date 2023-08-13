a5a4bec04c81b23a0965a1fee0b3d026d733df0e
snowlight-aemt <it.s.aemt@gmail.com>
2023-08-13 13:06:43 +0900

    맴버 생성, 조회, 수정 Service 분리

--- membership-service/src/main/java/com/snowlightpay/membership/application/service/FindMembershipService.java
+++ membership-service/src/main/java/com/snowlightpay/membership/application/service/FindMembershipService.java
@@ -3,27 +3,22 @@ package com.snowlightpay.membership.application.service;
 import com.snowlightpay.common.UseCase;
 import com.snowlightpay.membership.adapter.out.persistence.MembershipJpaEntity;
 import com.snowlightpay.membership.adapter.out.persistence.MembershipMapper;
-import com.snowlightpay.membership.application.port.in.*;
+import com.snowlightpay.membership.application.port.in.FindMembershipCommand;
+import com.snowlightpay.membership.application.port.in.FindMembershipUseCase;
 import com.snowlightpay.membership.application.port.out.RegisterMembershipPort;
 import com.snowlightpay.membership.domain.Membership;
 import lombok.RequiredArgsConstructor;
 
 @UseCase
 @RequiredArgsConstructor
-public class RegisterMembershipService implements RegisterMembershipUseCase {
+public class FindMembershipService implements FindMembershipUseCase {
     private final RegisterMembershipPort registerMembershipPort;
     private final MembershipMapper membershipMapper;
 
     @Override
-    public Membership createMembership(RegisterMembershipCommand  command) {
-        MembershipJpaEntity membership = registerMembershipPort.createMembership(
-                new Membership.MembershipName(command.getName()),
-                new Membership.MembershipEmail(command.getEmail()),
-                new Membership.MembershipAddress(command.getAddress()),
-                new Membership.MembershipValid(command.isValid()),
-                new Membership.MembershipCorp(command.isCorp())
-        );
-
+    public Membership findMembershipByMembershipId(FindMembershipCommand command) {
+        MembershipJpaEntity membership = registerMembershipPort.findMemberByMembershipId(
+                new Membership.MembershipId(command.getMembershipId()));
         return membershipMapper.mapToDomainEntity(membership);
     }
 }
--- membership-service/src/main/java/com/snowlightpay/membership/application/service/ModifyMembershipService.java
+++ membership-service/src/main/java/com/snowlightpay/membership/application/service/ModifyMembershipService.java
@@ -3,20 +3,22 @@ package com.snowlightpay.membership.application.service;
 import com.snowlightpay.common.UseCase;
 import com.snowlightpay.membership.adapter.out.persistence.MembershipJpaEntity;
 import com.snowlightpay.membership.adapter.out.persistence.MembershipMapper;
-import com.snowlightpay.membership.application.port.in.*;
+import com.snowlightpay.membership.application.port.in.ModifyMembershipCommand;
+import com.snowlightpay.membership.application.port.in.ModifyMembershipUseCase;
 import com.snowlightpay.membership.application.port.out.RegisterMembershipPort;
 import com.snowlightpay.membership.domain.Membership;
 import lombok.RequiredArgsConstructor;
 
 @UseCase
 @RequiredArgsConstructor
-public class RegisterMembershipService implements RegisterMembershipUseCase {
+public class ModifyMembershipService implements ModifyMembershipUseCase {
     private final RegisterMembershipPort registerMembershipPort;
     private final MembershipMapper membershipMapper;
 
     @Override
-    public Membership createMembership(RegisterMembershipCommand  command) {
-        MembershipJpaEntity membership = registerMembershipPort.createMembership(
+    public Membership modifyMembership(ModifyMembershipCommand command) {
+        MembershipJpaEntity membershipJpaEntity = registerMembershipPort.modifyMembership(
+                new Membership.MembershipId(command.getId()),
                 new Membership.MembershipName(command.getName()),
                 new Membership.MembershipEmail(command.getEmail()),
                 new Membership.MembershipAddress(command.getAddress()),
@@ -24,6 +26,6 @@ public class RegisterMembershipService implements RegisterMembershipUseCase {
                 new Membership.MembershipCorp(command.isCorp())
         );
 
-        return membershipMapper.mapToDomainEntity(membership);
+        return membershipMapper.mapToDomainEntity(membershipJpaEntity);
     }
 }
