96dca361a4e4bde622d00e7756bbaf7919f39b35
snowlight-aemt <it.s.aemt@gmail.com>
2023-10-08 11:18:01 +0900

    DynamoDB 도커에 제거

--- remittance-service/src/main/java/com/snowlightpay/remittance/adapter/out/persistance/RemittanceRequestJpaEntity.java
+++ remittance-service/src/main/java/com/snowlightpay/remittance/adapter/out/persistance/RemittanceRequestJpaEntity.java
@@ -2,13 +2,11 @@ package com.snowlightpay.remittance.adapter.out.persistance;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Data
+@Table(name = "remittance_request")
 public class RemittanceRequestJpaEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
